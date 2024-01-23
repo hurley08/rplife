8f71e662d4aed4da1a86e3cc57243a9b1b2fc494
hurley08 <intriqet@gmail.com>
2024-01-23 01:39:34 -0800

    parametrized pytest cases

--- test_rplife.py
+++ test_rplife.py
@@ -2,6 +2,16 @@ from __future__ import annotations
 
 import pytest
 
+pat_name = [
+    'Blinker',
+    'Toad',
+    'Pulsar',
+    'Beacon',
+    'Bunnies',
+    'Glider',
+    'Glider Gun',
+]
+
 
 def test_imports():
     try:
@@ -35,8 +45,16 @@ def CursesView():
 
 
 @pytest.fixture
+def pytest_set():
+    val_lits = patterns_set()
+    val = ('test_pat', val_lits)
+    return val
+
+
+@pytest.fixture
 def patterns_set():
-    return ['Pulsar', 'Beacon', 'Toad', 'Blinker']
+    patterns_set = ['Pulsar', 'Beacon', 'Toad', 'Blinker']
+    return patterns_set
 
 
 def test_load_modules():
@@ -49,20 +67,27 @@ def test_load_modules():
     assert True
 
 
+@pytest.mark.parametrize('p_names', pat_name)
+def test_lifegrid(get_pattern, patterns_set, p_names):
+    from rplife.grid import LifeGrid
+    p = LifeGrid(get_pattern(p_names))
+    print(str(p))
+    print(p.as_string)
+    assert True
+
+
 def test_get_all_patterns(patterns, get_pattern):
     result = patterns.get_all_patterns()
     assert (type(result) is list)
     assert (type(result[0]) != 'rplife.patterns.Pattern')
 
 
-def test_get_single_patterns(patterns, get_pattern, patterns_set):
-    for i in patterns_set:
-        res = patterns.get_pattern(i)
-        assert str(type(res)) == "<class 'rplife.patterns.Pattern'>"
-    for i in patterns_set:
-        res = get_pattern(i)
-        assert str(type(res)) == "<class 'rplife.patterns.Pattern'>"
-    assert True
+@pytest.mark.parametrize('p_name', pat_name)
+def test_get_single_patterns(patterns, get_pattern, p_name):
+    res = patterns.get_pattern(p_name)
+    assert str(type(res)) == "<class 'rplife.patterns.Pattern'>"
+    res = get_pattern(p_name)
+    assert str(type(res)) == "<class 'rplife.patterns.Pattern'>"
 
 
 def test_alive_cells_blinker(patterns):
@@ -70,23 +95,53 @@ def test_alive_cells_blinker(patterns):
     assert blink.alive_cells == {(2, 3), (2, 1), (2, 2)}
 
 
-def test_cursesview_calls(CursesView, get_pattern, patterns_set):
+@pytest.mark.parametrize(
+    'test_pattern',
+    [
+        'Blinker',
+        'Toad',
+        'Pulsar',
+        'Beacon',
+        'Bunnies',
+        'Glider',
+        'Glider Gun',
+    ],
+)
+def test_cursesview_calls(CursesView, get_pattern, test_pattern):
+    #    for i in patterns_set:
+    d = CursesView(get_pattern(test_pattern)).show()
+    assert d == False or d == None
+
+
+@pytest.mark.parametrize(
+    'f_rates',
+    [
+        5,
+        25,
+        100,
+        500,
+        1000,
+    ],
+)
+def test_cursesview_rates(CursesView, get_pattern, patterns_set, f_rates):
+    #   f_rates = [5, 25, 100, 500]
     for i in patterns_set:
-        d = CursesView(get_pattern(i)).show()
+        d = CursesView(get_pattern(i), frame_rate=f_rates).show()
         assert d == False or d == None
 
 
-def test_cursesview_rates(CursesView, get_pattern, patterns_set):
-    f_rates = [5, 25, 100, 500]
+@pytest.mark.parametrize(
+    'gens',
+    [
+        5,
+        25,
+        100,
+        1000,
+        2000,
+    ],
+)
+def test_cursesview_gen(CursesView, get_pattern, patterns_set, gens):
+    #    f_rates = [5, 25, 100, 2000]
     for i in patterns_set:
-        for j in f_rates:
-            d = CursesView(get_pattern(i), frame_rate=j).show()
-            assert d == False or d == None
-
-
-def test_cursesview_gen(CursesView, get_pattern, patterns_set):
-    f_rates = [5, 25, 100, 2000]
-    for i in patterns_set:
-        for j in f_rates:
-            d = CursesView(get_pattern(i), frame_rate=200, gen=j).show()
-            assert d == False or d == None
+        d = CursesView(get_pattern(i), frame_rate=200, gen=gens).show()
+        assert d == False or d == None
