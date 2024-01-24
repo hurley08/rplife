552f99c8816c353271d7fdbac390731fcf3ebe67
hurley08 <intriqet@gmail.com>
2024-01-23 22:19:03 -0800

    xfailed CursesViews cases

--- rplife/views.py
+++ rplife/views.py
@@ -1,36 +1,70 @@
+"""
+Implements CursesVIew which uses curses to draw the states of patterns
+"""
 from __future__ import annotations
 
 import curses
+import sys
 from time import sleep
 
 from rplife.grid import LifeGrid
+from rplife.patterns import Pattern
+
+
+__all__ = ['CursesView']
 
 
 class CursesView:
-    def __init__(self, pattern, gen=10, frame_rate=20, bbox=(0, 0, 20, 20)):
+    """
+    Draws visuals of pattern in its current state
+    """
+
+    def __init__(
+        self,
+        pattern: Pattern,
+        gen: int = 10,
+        frame_rate: int = 20,
+        bbox: tuple = (0, 0, 40, 80),
+    ) -> None:
+        """Initialize"""
         self.pattern = pattern
         self.gen = gen
         self.frame_rate = frame_rate
         self.bbox = bbox
 
-    def show(self):
+    def show(self) -> bool:
+        """Wraps _draw"""
         try:
             curses.wrapper(self._draw)
             return True
+        except OSError as strerror:
+            print(f'I/O error({strerror})')
+        except ValueError:
+            print('Could not convert data to an integer.')
         except:
-            return False
+            print('Unexpected error:', sys.exc_info()[0])
+            raise
+        return False
 
     def _draw(self, screen):
+        """Draws a window with dimensions of bbox"""
         current_grid = LifeGrid(self.pattern)
         curses.curs_set(0)
         screen.clear()
 
         try:
             screen.addstr(0, 0, current_grid.as_string(self.bbox))
-        except curses.error:
-            raise ValueError(
-                f"Error: terminal too small for pattern '{self.pattern.name}'",
-            )
+            print(f'Success in {self.bbox=}')
+        except OSError as strerror:
+            print(f'I/O error({strerror})')
+            screen.refresh()
+        except ValueError:
+            print('Could not convert data to an integer.')
+            screen.refresh()
+        except:
+            print('Unexpected error:', sys.exc_info()[0])
+            screen.refresh()
+            raise
 
         for _ in range(self.gen):
             current_grid.evolve()
